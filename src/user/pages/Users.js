a9341cec867095446dad321bab8a98d59672b235
Max Stevenson <maxstevenson@msn.com>
2020-02-04 11:00:21 +0000

    Fixed typo in UsersList.css
    - Created UserItem component.
    - Styling classes added.
    - Test User created for testing.

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React from "react";
+import UsersList from "../components/UsersList";
 
 const Users = () => {
-  return <h2>Users works!</h2>
+  const USERS = [
+    {
+      id: "u1",
+      name: "Test User",
+      image: "blank.url",
+      places: 3
+    }
+  ];
+
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
