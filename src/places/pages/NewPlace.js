5f9704aa681757cfa05aca59e878c66e7b978ce1
Max Stevenson <maxstevenson@msn.com>
2020-02-07 10:46:39 +0000

    Added state to Input component.

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,10 +1,18 @@
 import React from "react";
+import "./NewPlace.css";
+import Input from "../../shared/components/FormElements/Input";
 
 const NewPlace = () => {
   return (
-  <form className="place-form">
-    
-  </form>
+    <form className="place-form">
+      <Input
+        element="input"
+        type="text"
+        label="Title"
+        validators={[]}
+        errorText={"please enter valid title"}
+      />
+    </form>
   );
 };
 
