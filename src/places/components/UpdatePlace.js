c26c09e6b26ea048df6565ac8d01636af061e8e8
Max Stevenson <maxstevenson@msn.com>
2020-02-07 12:20:10 +0000

    Basic component created for updating existing places.

---
+++ src/places/components/UpdatePlace.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import Input from "../../shared/components/FormElements/Input";
+import Button from "../../shared/components/FormElements/Button";
+import {
+  VALIDATOR_REQUIRE,
+  VALIDATOR_MINLENGTH
+} from "../../shared/util/validators";
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "Very famous building in New York",
+    imageUrl:
+      "https://untappedcities.com/wp-content/uploads/2015/07/Flatiron-Building-Secrets-Roof-Basement-Elevator-Sonny-Atis-GFP-NYC_5.jpg",
+    address: "20 W 34th St, New York, NY 10001, United States",
+    location: [-73.9856644, 40.7484405],
+    creator: "u1"
+  },
+  {
+    id: "p2",
+    title: "Empire State Building",
+    description: "Very famous building in New York",
+    imageUrl:
+      "https://untappedcities.com/wp-content/uploads/2015/07/Flatiron-Building-Secrets-Roof-Basement-Elevator-Sonny-Atis-GFP-NYC_5.jpg",
+    address: "20 W 34th St, New York, NY 10001, United States",
+    location: [-73.9856644, 40.7484405],
+    creator: "u2"
+  }
+];
+
+const UpdatePlace = () => {
+  const placeId = useParams().placeId;
+  const identifiedPlace = DUMMY_PLACES.find(place => place.id === placeId);
+
+  if (!identifiedPlace) {
+    return (
+      <div className="center">
+        <h2>Could not find place</h2>
+      </div>
+    );
+  }
+
+  return (
+    <form>
+      <Input
+        id="title"
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        onInput={() => {}}
+        value={identifiedPlace.title}
+        valid={true}
+      />
+      <Input
+        id="description"
+        element="textarea"
+        label="Description"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        onInput={() => {}}
+        value={identifiedPlace.description}
+        valid={true}
+      />
+      <Button type="submit" disabled={true}>UPDATE</Button>
+    </form>
+  );
+};
+
+export default UpdatePlace;
