f1039f2643f59fd9b4559f32bc799011c7a76efd
Max Stevenson <maxstevenson@msn.com>
2020-02-14 13:48:58 +0000

    HttpError model created. Refactor of existing error handling.

--- backend/src/routes/places-routes.js
+++ backend/src/routes/places-routes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const HttpError = require('../models/http-error');
 
 const DUMMY_PLACES = [
   {
@@ -31,9 +32,7 @@ router.get("/:pid", (req, res, next) => {
   });
 
   if (!place) {
-    const error = new Error("Could not find a place for the provided id.");
-    error.code = 404;
-    return next(error);
+    return next(new HttpError("Could not find a place for the provided id.", 404));
   }
 
   res.json({ place });
@@ -46,9 +45,7 @@ router.get("/user/:uid", (req, res, next) => {
   });
 
   if (!place) {
-    const error = new Error("Could not find a place for the provided user id.");
-    error.code = 404;
-    return next(error);
+    return next(new HttpError("Could not find a place for the provided userid.", 404));
   }
 
   res.json({ place });
