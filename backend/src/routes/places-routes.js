5391b9730800f20ddca9704a60b153242395c892
Max Stevenson <maxstevenson@msn.com>
2020-02-16 13:59:04 +0000

    Update place route and logic created.

--- backend/src/routes/places-routes.js
+++ backend/src/routes/places-routes.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const placesControllers = require('../controllers/places-controllers');
+const placesControllers = require("../controllers/places-controllers");
 
 router.get("/:pid", placesControllers.getPlaceById);
 
 router.get("/user/:uid", placesControllers.getPlaceByUserId);
 
-router.post('/', placesControllers.createPlace);
+router.post("/", placesControllers.createPlace);
+
+router.patch("/:pid", placesControllers.updatePlace);
+
+router.delete("/:pid", placesControllers.deletePlace);
 
 module.exports = router;
