952013507cce8650ccbe7cf36430a310935ec2e3
Max Stevenson <maxstevenson@msn.com>
2020-02-20 21:36:28 +0000

    Added error handling for geocode util.

--- backend/src/utils/geocode.js
+++ backend/src/utils/geocode.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const HttpError = require("../models/http-error");
 const MAPBOX_API = process.env.MAPBOX_API;
 const axios = require("axios");
 
@@ -9,9 +10,16 @@ async function geocode(address) {
     ".json?access_token=" +
     MAPBOX_API;
   const res = await axios.get(geocoderUrl).then(res => res.data);
+  if (res.features.length === 0) {
+    return next(new HttpError("Could not find coordinates for address", 400));
+  }
   const point = res.features[0].geometry;
 
   return point;
 }
+
+geocode("thlkjslfj").then(res => {
+  console.log(res);
+});
 
 module.exports = geocode;
