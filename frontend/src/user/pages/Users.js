f70384de2d2bcaf0622e8442e74cb5404ebf499f
Max Stevenson <maxstevenson@msn.com>
2020-03-10 23:02:41 +0000

    finished implementing custom http hook in Users.

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -9,32 +9,23 @@ const Users = () => {
   const [loadedUsers, setLoadedUsers] = useState();
 
   useEffect(() => {
-    const sendRequest = async () => {
-      setIsLoading(true);
+    const fetchUsers = async () => {
       try {
-        const response = await fetch("http://localhost:5000/api/users");
-        const responseData = await response.json();
-
-        if (!response.ok) {
-          throw new Error(responseData.message);
-        }
+        const responseData = await sendRequest(
+          "http://localhost:5000/api/users"
+        );
 
         setLoadedUsers(responseData.users);
       } catch (err) {
         setError(err.message);
       }
-      setIsLoading(false);
     };
-    sendRequest();
-  }, []);
-
-  const errorHandler = () => {
-    setError(null);
-  };
+    fetchUsers();
+  }, [sendRequest]);
 
   return (
     <React.Fragment>
-      <ErrorModal error={error} onClear={errorHandler} />
+      <ErrorModal error={error} onClear={clearError} />
       {isLoading && (
         <div className="center">
           <LoadingSpinner />
