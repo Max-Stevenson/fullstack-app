691389cf794b964b430fd83861fa487e0dc1503e
Max Stevenson <maxstevenson@msn.com>
2020-03-28 23:52:12 +0000

    more WIP on image upload.

--- frontend/src/shared/components/FormElements/ImageUpload.js
+++ frontend/src/shared/components/FormElements/ImageUpload.js
@@ -1,8 +1,22 @@
-import React from 'react';
-import './ImageUpload.css'
+import React from "react";
+import Button from "./Button";
+import "./ImageUpload.css";
 
 const ImageUpload = props => {
-
+  return (
+    <div className="form-control">
+      <input
+        type="file"
+        id={props.id}
+        style={{ display: "none" }}
+        accept=".jpg,.png,.jpeg"
+      />
+      <div className={`image-upload ${props.center && "center"}`}>
+        <div className="image-upload__preview">
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ImageUpload;
