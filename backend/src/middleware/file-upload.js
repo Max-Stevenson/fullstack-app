2cfc5368bcafdc99f338bf65cbb09339f9377840
Max Stevenson <maxstevenson@msn.com>
2020-03-31 11:15:36 +0100

    Finished multer config.

--- backend/src/middleware/file-upload.js
+++ backend/src/middleware/file-upload.js
@@ -11,13 +11,18 @@ const fileUpload = multer({
   limits: 500000,
   storage: multer.diskStorage({
     destination: (req, file, cb) => {
-
+      cb(null, 'uploads/images');
     },
     filename: (req, file, cb) => {
       const ext = MIME_TYPE_MAP[file.mimetype];
       cb(null, uuid() + '.' + ext);
     }
-  })
+  }),
+  fileFilter: (req, file, cb) => {
+    const isValid = !!MIME_TYPE_MAP[file.mimetype];
+    let err = isValid ? null : new Error('Invalid mime type');
+    cb(err, isValid);
+  }
 });
 
 module.exports = fileUpload;
